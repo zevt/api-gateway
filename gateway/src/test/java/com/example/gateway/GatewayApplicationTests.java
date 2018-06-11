2b594bd31adbd62fedc2a6b0fc8519e497167a5e
Viet <vietsemantics@gmail.com>
2018-06-11 00:50:21 -0500

    init service a and b

--- gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.apigateway;
+package com.example.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApiGatewayApplicationTests {
+public class GatewayApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+  @Test
+  public void contextLoads() {}
 }
